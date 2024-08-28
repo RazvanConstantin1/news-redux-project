d2e149acb3fe9670cc87e21c2b9c1dd210e95e4a
RazvanConstantin1 <razvanconstantin1097@gmail.com>
2024-08-28 21:21:17 +0200

    made and styled the login page

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,8 +1,49 @@
 import React from "react";
 import styles from "../styles/Login.module.css";
+import { useNavigate } from "react-router-dom";
+import { useState } from "react";
 
 const Login = () => {
-  return <div>Login</div>;
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const navigate = useNavigate();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    navigate("/");
+
+    setEmail("");
+    setPassword("");
+  };
+
+  return (
+    <div className={styles.formWrapper}>
+      <form onSubmit={handleSubmit} className={styles.form}>
+        <div className={styles.inputWrapper}>
+          <label htmlFor="email">Email</label>
+          <input
+            type="email"
+            id="email"
+            required
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </div>
+        <div className={styles.inputWrapper}>
+          <label htmlFor="password">Password</label>
+          <input
+            type="password"
+            id="password"
+            required
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+        <button type="submit">Sign In</button>
+      </form>
+    </div>
+  );
 };
 
 export default Login;
